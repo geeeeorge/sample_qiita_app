8004f26fd778455496cdc984153f4c777d1bf3c6
geeeeorge <yuto.george@i.softbank.jp>
2023-05-14 16:57:30 +0900

    :recycle: QiitaItemListページに移行

---
+++ src/pages/QiitaItemList.tsx
@@ -0,0 +1,25 @@
+import { GridRowsProp } from '@mui/x-data-grid'
+import type { NextPage } from 'next'
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { getQiitaItems, QiitaItems } from '../api/QiitaApi'
+import SearchAppBar from '../components/AppBar'
+import QiitaItemsGrid from '../components/QiitaItemsGrid'
+
+const QiitaItemList: NextPage = () => {
+  const [resRows, setResRows] = useState<QiitaItems>([])
+
+  useEffect(() => {
+    getQiitaItems(setResRows, '')
+  }, [])
+  const rows: GridRowsProp = resRows
+
+  return (
+    <>
+      <SearchAppBar />
+      <QiitaItemsGrid rows={rows} />
+    </>
+  )
+}
+
+export default QiitaItemList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
-import { GridRowsProp } from '@mui/x-data-grid'
 import type { NextPage } from 'next'
 import * as React from 'react'
-import { useState, useEffect } from 'react'
-import { getQiitaItems, QiitaItems } from '../api/QiitaApi'
-import SearchAppBar from '../components/AppBar'
-import QiitaItemsGrid from '../components/QiitaItemsGrid'
+import QiitaItemList from '../pages/QiitaItemList'
 
 const Home: NextPage = () => {
-  const [resRows, setResRows] = useState<QiitaItems>([])
-
-  useEffect(() => {
-    getQiitaItems(setResRows)
-  }, [])
-  const rows: GridRowsProp = resRows
-
   return (
     <>
-      <SearchAppBar />
-      <QiitaItemsGrid rows={rows} />
+      <QiitaItemList />
     </>
   )
 }
