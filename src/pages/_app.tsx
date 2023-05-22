89995e79a568746d287102bade6e671ec11604ce
geeeeorge <yuto.george@i.softbank.jp>
2023-05-23 00:32:18 +0900

    :lipstick: 色を黄色に

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { ApiKeyProvider } from '@/pages/index'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFD700',
+    },
+  },
+})
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApiKeyProvider>
-     <Component {...pageProps} />
-    </ApiKeyProvider>
+    < ThemeProvider theme={theme}>
+      <ApiKeyProvider>
+        <Component {...pageProps} />
+      </ApiKeyProvider>
+    </ThemeProvider>
   )
 }
